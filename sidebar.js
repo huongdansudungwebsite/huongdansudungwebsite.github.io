a07e53054db6012401b8f3a7dd9aeeead1381f77
huongdansudungwebsite <39013039+huongdansudungwebsite@users.noreply.github.com>
2025-05-20 15:39:42 +0700

    Update sidebar.js

--- sidebar.js
+++ sidebar.js
@@ -3,14 +3,34 @@ const sidebar = {
 		'text': 'Home',
 		'href': '/',
 	},
+	setting :{
+		'text': 'Cài đặt chung',
+		'href': '#setting',
+	},
 	post :{
 		'text': 'Bài viết',
 		'href': '#post',
 	},
+	page :{
+		'text': 'Trang',
+		'href': '#page',
+	},
+	product :{
+		'text': 'Sản phẩm',
+		'href': '#product',
+	},
 	slide :{
 		'text': 'Slide',
 		'href': '#slide',
-	}
+	},
+	navigation :{
+		'text': 'Điều hướng',
+		'href': '#navigation',
+	},
+	user :{
+		'text': 'User',
+		'href': '#user',
+	},
 }
 
 
